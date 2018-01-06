c55c667c54e4202bf5378de4d991c9c282396772
Harrison <Isaiah_Harrison@comcast.com>
2018-01-06 06:31:53 -0500

    cleaned up navigation javascript

--- js/main.js
+++ js/main.js
@@ -5,22 +5,17 @@ var app = angular.module('app', []);
 app.controller('ctrl', ['$scope', 'navigate', 'data', function($scope, navigate, data){
   $scope.navigationPoints = data.navigationPoints;
   $scope.bringToTopOfPage = (e, dataValue) => {
-    debugger
     (dataValue === undefined) ? navigate.toTag(e) : navigate.toTagWithDataValue(dataValue);
   }
 }])
 
 app.service('navigate', function(){
   this.toTag = (e) => {
-    const attributes = e.currentTarget.attributes;
-    const dataAttributeNodeValue = attributes.data.nodeValue;
-    const selector = $(".textHeading[data=" + dataAttributeNodeValue + "]");
-    const offsetTop = parseInt(selector[0].offsetTop);
-    const element = document.getElementById('mainContent');
-    element.scrollTop = offsetTop - 140;
+    const dataValue = e.currentTarget.attributes.data.nodeValue;
+    this.toTagWithDataValue(dataValue);
   }
   this.toTagWithDataValue = (dataValue) => {
-    const selector = $("div[data=" + dataValue + "]");
+    const selector = $(".textHeading[data=" + dataValue + "]");
     const offsetTop = parseInt(selector[0].offsetTop);
     const element = document.getElementById('mainContent');
     element.scrollTop = offsetTop - 140;
