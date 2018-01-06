79c2f223cb05cac70ea1a9e1e64a6a7cc5447640
Harrison <Isaiah_Harrison@comcast.com>
2018-01-06 11:43:54 -0500

    animation box fixed

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@
 
 var app = angular.module('app', []);
 
-app.controller('ctrl', ['$scope', 'navigate', 'data', function($scope, navigate, data){
+app.controller('ctrl', ['$rootScope', '$scope', 'navigate', 'data', 'task', function($rootScope, $scope, navigate, data, task){
+  $rootScope.url = 'yourwebsite.com';
   $scope.navigationPoints = data.navigationPoints;
   $scope.bringToTopOfPage = (e, dataValue) => {
-    (dataValue === undefined) ? navigate.toTag(e) : navigate.toTagWithDataValue(dataValue);
+    (dataValue === undefined) ? navigate.toTagWithEvent(e) : navigate.toTagWithDataValue(dataValue);
   }
+  task.typeUrl();
 }])
 
 app.service('navigate', function(){
-  this.toTag = (e) => {
+  this.toTagWithEvent = (e) => {
     const dataValue = e.currentTarget.attributes.data.nodeValue;
     const selector = $(".textHeading[data=" + dataValue + "]");
     this.navigateTo(selector);
@@ -34,4 +36,37 @@ app.service('data', function(){
     {data: 3, point: 'example'},
     {data: 4, point: 'contact'}
   ]
+})
+
+app.service('task', function($rootScope, $timeout){
+  this.typeUrl = () => {
+    $('.pageContent').hide();
+    let delayTimes = [100, 200, 300, 400];
+    let i = 1;
+    let didntEraseYet = true;
+    const $selector = $(".url p");
+    const typeText = () => {
+      let text;
+      if((i === 4) && didntEraseYet){
+        i--;
+        text = $rootScope.url.slice(0, i);
+        didntEraseYet = false;
+      } else {
+        text = $rootScope.url.slice(0, i);
+      }
+      if(i === $rootScope.url.length + 1){
+        console.log('done');
+        $('.urlCircle').css('opacity', 0.6).addClass('urlClicked');
+        $timeout(() => { $('.urlCircle').css('opacity', 1).removeClass('urlClicked') }, 200);
+        $timeout(() => { $('.pageContent').fadeIn() }, 500);
+      } else {
+        i++;
+        $selector.text(text);
+        const delay = (Math.floor(Math.random() * 3) + 1) * 100;
+        $timeout(() => { typeText() }, delay);
+      }
+
+    }
+    $timeout(() => { typeText() }, 1000);
+  }
 })
